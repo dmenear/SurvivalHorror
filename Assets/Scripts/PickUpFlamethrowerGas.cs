23d9c5bfa3f94da7388b566efa8b9fb4a07d30df
jdean5 <jsdean14@gmail.com>
2018-04-08 06:52:46 -0400

    gas container pickup

---
+++ Assets/Scripts/PickUpFlamethrowerGas.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpFlamethrowerGas : MonoBehaviour
+{
+
+    public GameObject player, textBox, flamethrowerGasMesh;
+    public AudioSource audio;
+    public float distance, angle;
+    public bool onShelf = false;
+    public bool pickedUp = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.transform.position - this.transform.position;
+        angle = Vector3.Angle(direction, player.transform.forward);
+        distance = direction.magnitude;
+        if (angle >= 160 && distance <= (onShelf ? 2.3f : 2f) && !pickedUp)
+        {
+            if (Input.GetButtonDown("Action"))
+            {
+                audio.Play();
+                displayMessage();
+                flamethrowerGasMesh.GetComponent<MeshRenderer>().enabled = false;
+                StartCoroutine(displayMessage());
+                pickedUp = true;
+            }
+        }
+    }
+
+    IEnumerator displayMessage()
+    {
+        textBox.GetComponent<Text>().text = "You found a flamethrower gas tank.";
+        yield return new WaitForSecondsRealtime(3.0f);
+        textBox.GetComponent<Text>().text = "";
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PickupGasContainers.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickupGasContainers : MonoBehaviour
+{
+
+    public GameObject player, textBox, containerMesh;
+    public AudioSource audio;
+    public float distance, angle;
+    public bool onFloor = false;
+    public bool pickedUp = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.transform.position - this.transform.position;
+        angle = Vector3.Angle(direction, player.transform.forward);
+        distance = direction.magnitude;
+        if (angle >= 160 && distance <= (onFloor ? 2.3f : 2f) && !pickedUp)
+        {
+            if (Input.GetButtonDown("Action"))
+            {
+                audio.Play();
+                displayMessage();
+                containerMesh.GetComponent<MeshRenderer>().enabled = false;
+                StartCoroutine(displayMessage());
+                pickedUp = true;
+            }
+        }
+    }
+
+    IEnumerator displayMessage()
+    {
+        textBox.GetComponent<Text>().text = "You found a gas source.";
+        yield return new WaitForSecondsRealtime(3.0f);
+        textBox.GetComponent<Text>().text = "";
+        gameObject.SetActive(false);
+    }
+}
