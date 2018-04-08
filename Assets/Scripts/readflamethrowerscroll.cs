a1d247d8bbf71501868c148ff7386073784af63f
jdean5 <jsdean14@gmail.com>
2018-04-08 04:04:45 -0400

    added sound to scroll

--- Assets/Scripts/readflamethrowerscroll.cs
+++ Assets/Scripts/readflamethrowerscroll.cs
@@ -6,19 +6,23 @@ public class readflamethrowerscroll : MonoBehaviour {
 
 	public GameObject player, flameThrowerScroll;
 	public float distance, angle;
+    public AudioSource efxSource;
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 		Vector3 direction = player.transform.position - this.transform.position;
 		angle = Vector3.Angle (direction, player.transform.forward);
 		distance = direction.magnitude;
 		if (angle >= 160 && distance <= 2 && !flameThrowerScroll.activeSelf) {
 			if(Input.GetButtonDown("Action")) {
 				flameThrowerScroll.SetActive (true);
+                efxSource.Play();
 			}
 		}
 		else if (flameThrowerScroll.activeSelf && (Input.GetButtonDown("Action") || Input.anyKeyDown)) {
 			flameThrowerScroll.SetActive (false);
+       
 		}
 	}
+ 
 }
